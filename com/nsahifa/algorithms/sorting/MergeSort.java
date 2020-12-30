8764570ca6cb3459887593a7e2daeb79c89687c1
Nabi SAHIFA <36372509+NSAHIFA@users.noreply.github.com>
2020-12-30 17:48:11 +0100

    Divide and Conquer : Merge Sort + Inversions count

--- com/nsahifa/algorithms/sorting/MergeSort.java
+++ com/nsahifa/algorithms/sorting/MergeSort.java
@@ -1,5 +1,55 @@
+import java.util.Arrays;
+
 /**
  * Implementation of Merge Sort
+ * Time complexity : O(nlogn) : the algorithm divides the array
+ * into two halves and take linear time to merge two halves
  */
 public class MergeSort {
+
+    public static void mergeSort(int[] arr, int start, int end) {
+        if (start < end) {
+            int mid = (start+end)/2;
+
+            // sort the left subarray
+            mergeSort(arr, start, mid);
+            // sort the right subarray
+            mergeSort(arr, mid+1, end);
+
+            // merge the two subarrays
+            merge(arr, start, mid, end);
+        }
+    }
+
+    private static void merge(int[] arr, int start, int mid, int end) {
+
+        // stores the starting positions of both subarrays
+        int p = start, q = mid+1;
+        int pos = 0;
+        int[] tmp = new int[end-start+1];
+
+        for (int i=start; i<=end; ++i) {
+            if (p>mid) {
+                // The first subarray comes to an end
+                tmp[pos++] = arr[q++];
+            }
+            else if (q>end) {
+                // The second subarray comes to an end
+                tmp[pos++] = arr[p++];
+            }
+            else if (arr[p]<arr[q]) {
+                // the left subarray has smaller element
+                tmp[pos++] = arr[p++];
+            }
+            else {
+                tmp[pos++] = arr[q++];
+            }
+
+            for (int k=0; k<pos; ++k) {
+                // copy the sorted array back to the orginal one
+                arr[start++] = tmp[k++];
+            }
+        }
+    }
+
 }
---
+++ com/nsahifa/algorithms/sorting/NumberOfInversions.java
@@ -0,0 +1,66 @@
+/**
+ * Implementation of the algorithm for counting the number of inversions
+ *
+ * Reminder : Let A[0 ... n-1] be an array of n distinct positive integers.
+ * If i < j and A[i] > A[j] then the pair (i, j) is called an inversion of A
+ */
+public class NumberOfInversions {
+
+    public static int mergeSort(int[] arr, int start, int end) {
+
+        // Inversion count will be the sum of the inversions in the left subarray,
+        // the right subarray and the inversions in merging
+        int inversion_cnt = 0;
+        if (start < end) {
+
+            // divide the array into two parts
+            int mid = (start + end)/2;
+
+            // count the inversions in the left subarray
+            inversion_cnt += mergeSort(arr, start, mid);
+
+            // count the inversions in the right subarray
+            inversion_cnt += mergeSort(arr, mid+1, end);
+
+            // count the inversions while merging the two parts
+            inversion_cnt += merge(arr, start, mid, end);
+        }
+        return inversion_cnt;
+    }
+
+    // This function merges two sorted arrays and returns
+    // the inversion count in the merged array
+    private static int merge(int[] arr, int start, int mid, int end) {
+
+        int p = start, q = mid+1;
+        int pos = 0, inversion_cnt = 0;
+        int[] tmp = new int[end-start+1];
+
+        while ((p<=mid) && (q<=end)) {
+            if (arr[p] <= arr[q]){
+                tmp[pos++] = arr[p++];
+            }
+            else {
+                tmp[pos++] = arr[q++];
+
+                // there is (mid-p) inversions, because left and right subarray
+                // are sorted. So all the remaining elements in left subarray
+                // (arr[p+1),...,arr[mid]) will be greater than arr[q]
+                inversion_cnt += (mid - p);
+            }
+        }
+        while (p<=mid) {
+            tmp[pos++] = arr[p++];
+        }
+        while (q<=end) {
+            tmp[pos++] = arr[q++];
+        }
+
+        // copy back the merged elements to the original array
+        for (int k=start;k<=end;++k)
+            arr[k] =  tmp[k];
+
+        return inversion_cnt;
+    }
+
+}
